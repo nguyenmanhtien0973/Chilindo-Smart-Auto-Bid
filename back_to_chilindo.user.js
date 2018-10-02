75f7c1fb3ed88b0ddeff5498d8d12532276362a3
duyltv <ngocduy1842@gmail.com>
2018-10-02 17:01:50 +0700

    Update for v0.2

--- back_to_chilindo.user.js
+++ back_to_chilindo.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Back to chilindo
 // @namespace    http://commandlab.net
-// @version      0.1
+// @version      0.2
 // @description  Back to Chilindo incase error page appears
 // @author       DuyLTV
 // @match        http://www.chilindo.com/vn/error*
+// @match        https://www.chilindo.com/vn/error*
 // @grant        none
 // ==/UserScript==
 
